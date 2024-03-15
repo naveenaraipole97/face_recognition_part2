301bc61a96d4ae065790e8613d15449abd29ddd4
naveena <144768728+naveenaraipole97@users.noreply.github.com>
2024-03-14 20:11:44 -0700

    sending images as bytes to sqs and moved image upload to app tier

--- model/face_recognition.py
+++ model/face_recognition.py
@@ -11,6 +11,7 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 from torchvision import datasets
 from torch.utils.data import DataLoader
 import boto3
+import json
 from io import BytesIO
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError, EndpointConnectionError
 
@@ -60,21 +61,27 @@ def sendMessageToRespQueue(s3_file_name,result):
     logging.info(f"Message sent to response queue successfully. Message ID: {response['MessageId']}")
 
 
-def uploadToS3(s3_file_name,result):
+def uploadResultToS3(s3_file_name,result):
     try:
         s3.put_object(Bucket=out_bucket_name, Key=s3_file_name, Body=result)
         logging.info(f'response uploaded to S3 Successfully to {s3_file_name}')
-        sendMessageToRespQueue(s3_file_name,result)
+        # sendMessageToRespQueue(s3_file_name,result)
     except NoCredentialsError:
         logging.info('Credentials Not Available')
 
+def uploadImageToS3(file_name,byte_io):
+    try:
+       s3.put_object(Body=byte_io, Bucket=in_bucket_name, Key=file_name, ContentType='image/jpeg') 
+       logging.info(f"Image uploaded to S3 successfully to {file_name}")
+    except NoCredentialsError:
+        logging.info('Credentials Not Available')
 
-def face_match(image_data, data_path): # img_path= location of photo, data_path= location of data.pt
+def face_match(image_byte_array, data_path): # img_path= location of photo, data_path= location of data.pt
     # getting embedding matrix of the given img
-    image1=Image.open(BytesIO(image_data));
+    # image1=Image.open(BytesIO(image_data));
     # img = Image.open(test_image)
-    img=image1
-    print("s3 image",image1)
+    img=Image.open(image_byte_array)
+    # print("s3 image",image1)
     face, prob = mtcnn(img, return_prob=True) # returns cropped face and probability
     emb = resnet(face.unsqueeze(0)).detach() # detech is to make required gradient false
 
@@ -113,19 +120,31 @@ def process_images():
     
             if messages:
                 for message in messages:
-                    logging.info(f"Received message: Message body {message['Body']}")
-                    s3_file_path=message['Body']
-                    logging.info(s3_file_path)
+                    # logging.info(f"Received message: Message body {message['Body']}")
+
+                    req_message=message['Body']
+                    # s3_file_path=message['Body']
+                    # logging.info(s3_file_path)
     
-                    response=s3.get_object(Bucket=in_bucket_name,Key=s3_file_path)
-                    image_data=response['Body'].read();
+                    # response=s3.get_object(Bucket=in_bucket_name,Key=s3_file_path)
+                    # image_data=response['Body'].read();
+                    req_message=json.loads(req_message)
                     
-                    result=face_match(image_data,'data.pt')
-                    s3_file_name, extension=os.path.splitext(s3_file_path)
-                    uploadToS3(s3_file_name,result[0])
-                    
+                    #check if key is present
+                    file_name=req_message["key"]
+                    image_byte_array=req_message["value"]
+
+                    result=face_match(BytesIO(image_byte_array.encode('latin-1')),'data.pt')
                     logging.info(result)
-    
+
+                    fileName, extension=os.path.splitext(file_name)
+
+                    sendMessageToRespQueue(fileName,result[0])
+
+                    uploadResultToS3(fileName,result[0])
+
+                    uploadImageToS3(file_name,image_byte_array)
+                    
                     receipt_handle=message['ReceiptHandle']
                     sqs.delete_message(
                         QueueUrl=req_queue_url,
